71d547b0e115d4370eb5b30574da07358ab58b01
RyanOrsinger <ryan.orsinger@gmail.com>
2019-08-29 23:08:04 -0500

    add string interpolation example

---
+++ string_interpolation.jl
@@ -0,0 +1,11 @@
+name = "Ryan"
+
+language = "Julia"
+
+message = "Hi, my name is $name and I am practicing the $language language."
+
+println(message)
+
+another_language = "PHP"
+
+println("$language uses the same string interpolation syntax as $another_language")
