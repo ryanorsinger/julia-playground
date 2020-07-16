6a5e4fa3119cca9ec9919d8ca805706c18715737
RyanOrsinger <ryan.orsinger@gmail.com>
2020-07-15 20:15:25 -0500

    add wrapper todos and string basics

---
+++ playground.jl
@@ -0,0 +1,44 @@
+# # how to print
+# # assigning variables
+# # syntax for basic math
+
+# println("Print to Standard output")
+# 123
+
+# # the "ans" variable holds the last expression in memory
+
+# # basic math operations
+# sum = 3 + 8
+# difference = 10 - 3
+# product = 20 * 3
+# power = 10^2
+# println(product)
+
+s1 = "this is a string"
+s2 = """ this is another string"""
+
+# typeof(string)
+
+# typeof('a')
+# # typeof('aa')
+
+# # String Interpolation
+# # dollar sign to throw variables into a string
+# a = 5
+# print("The value of a is $a")
+# println()
+# b = 3
+# print("The sum of a and b is $(a + b)")
+
+# String concatenation
+# Method 1, use the string function
+string("con", "cat", "e", "nate")
+string("con", 3, "cat", 3, "e", 3, "nate")
+
+a = 1
+b = 2
+# Method 2 of string concatenation
+"$a$b"
+
+
+
---
+++ wrapper.jl
@@ -0,0 +1,6 @@
+# Goal is to extend the Julia API to be forgiving of mental typos using other language functions
+
+print = println
+len = length
+type = typeof
+gettype = typeof
